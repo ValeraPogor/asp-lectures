0a6c278f5bae971889090dad4e3420a46053f29e
Valerii Pogorzhelskyi <valera.pogor@gmail.com>
2015-05-24 14:19:48 +0300

    Form Commit
    Add new controller with view

---
+++ Feedback/Models/FeedbackModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Feedback.Models
+{
+    public class FeedbackModel
+    {
+        //private Guid Id { get; set; }
+
+        //public FeedbackModel()
+        //{
+        //    Id = Guid.NewGuid();
+        //}
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Display(Name = "Feedback")]
+        public string Feedback { get; set; }
+    }
+}
